02fa00c0e5e4f0e1ce1947e6097e99c89e129fcc
go-zip-88 <poogozip@gmail.com>
2023-06-24 20:43:39 +0900

    2023/06/24_security 적용

---
+++ src/main/java/com/jwt/jwtTest/Controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.jwt.jwtTest.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/api")
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello!");
+    }
+}
