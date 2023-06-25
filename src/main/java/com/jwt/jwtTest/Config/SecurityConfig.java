92ffe450c39bde3f795a40dfa2ad7d73c70495ec
go-zip-88 <poogozip@gmail.com>
2023-06-25 22:49:31 +0900

    2023/06/25_불필요코드 정리

--- src/main/java/com/jwt/jwtTest/Config/SecurityConfig.java
+++ src/main/java/com/jwt/jwtTest/Config/SecurityConfig.java
@@ -2,11 +2,8 @@ package com.jwt.jwtTest.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
